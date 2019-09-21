bc45a3e046c8404db02a016aec09c5862d24a2e6
Rafael Calhau <rafael@calhau.me>
2019-09-21 17:38:35 -0300

    Created generateToken and model pre-save handler to encrypt the user passowrd. Updated the UserInterface.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,17 @@
 import { Schema, model, Document } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 interface UserInterface extends Document {
   email: string;
-  firstname?: string;
-  fullname?: string;
-  lasname?: string;
+  firstname: string;
+  fullname?: Function;
+  lastname?: string;
+  password?: string;
+  generateToken: Function;
 }
 
-const UserSchema = new Schema({
+const UserSchema: Schema<UserInterface> = new Schema({
   email: {
     type: String,
     allowNull: false,
@@ -20,6 +24,10 @@ const UserSchema = new Schema({
   lastname: {
     type: String,
     allowNull: false
+  },
+  password: {
+    type: String,
+    allowNull: false
   }
 }, {
   timestamps: true
@@ -28,5 +36,17 @@ const UserSchema = new Schema({
 UserSchema.methods.fullname = function (): string {
   return `${this.firstname} ${this.lastname}`
 }
+
+UserSchema.methods.generateToken = function (): string {
+  return jwt.sign({ id: this.id }, process.env.JWT_SECRET)
+}
+
+UserSchema.pre<UserInterface>('save', async function (next): Promise<void> {
+  if (this.password) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+
+  return next()
+})
 
 export default model<UserInterface>('User', UserSchema)
